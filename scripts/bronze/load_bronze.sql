1f3b5ecb4cdbb30255745338e827467b16a383c3
Sanidhya Madhav Shukla <125867986+SanidhyaMadhavShukla@users.noreply.github.com>
2025-08-06 17:57:59 +0530

    Update load_bronze.sql

--- scripts/bronze/load_bronze.sql
+++ scripts/bronze/load_bronze.sql
@@ -3,7 +3,7 @@
 Insert Data into Bronze Layer Tables (DML Script)
 ------------------------------------
 Script Purpose:
-	This script creates and executes a procedure that inserts data into tables in the 'bronze' schema from source, it truncates tables before loading data.
+	This script creates a procedure that inserts data into tables in the 'bronze' schema from source, it truncates tables before loading data.
 
 WARNING:
 	Running this script will Truncate the tables leading to loss of all data stored in them.
@@ -14,60 +14,121 @@ GO
 
 CREATE OR ALTER PROCEDURE bronze.load_bronze AS
 BEGIN
-	--Load Data into Tables from Source
-	TRUNCATE TABLE bronze.crm_cust_info;
-	BULK INSERT bronze.crm_cust_info
-	FROM 'C:\Users\sanid\Desktop\365 Plan\Skills\Data Analytics\SQL\Data Warehouse Project (CRM-ERP Data)\Datasets\source_crm\cust_info.csv' 
-	WITH (
-		FIRSTROW = 2,
-		FIELDTERMINATOR = ',',
-		TABLOCK
-	);
+	DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME, @batch_end_time DATETIME
+	BEGIN TRY
+		SET @batch_start_time = GETDATE();
+		PRINT('==============================================================');
+		PRINT('Loading Bronze Layer');
+		PRINT('==============================================================');
 
-	TRUNCATE TABLE bronze.crm_prd_info;
-	BULK INSERT bronze.crm_prd_info
-	FROM 'C:\Users\sanid\Desktop\365 Plan\Skills\Data Analytics\SQL\Data Warehouse Project (CRM-ERP Data)\Datasets\source_crm\prd_info.csv' 
-	WITH (
-		FIRSTROW = 2,
-		FIELDTERMINATOR = ',',
-		TABLOCK
-	);
+		--Load Data into Tables from Source
+		PRINT('--------------------------------------------------------------');
+		PRINT('Loading CRM Tables');
+		PRINT('--------------------------------------------------------------');
+		SET @start_time = GETDATE();
+		PRINT('-> Truncating Table: bronze.crm_cust_info');
+		TRUNCATE TABLE bronze.crm_cust_info;
+		PRINT('-> Inserting Data Into: bronze.crm_cust_info');
+		BULK INSERT bronze.crm_cust_info
+		FROM 'C:\Users\sanid\Desktop\365 Plan\Skills\Data Analytics\SQL\Data Warehouse Project (CRM-ERP Data)\Datasets\source_crm\cust_info.csv' 
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		SET @end_time = GETDATE();
+		PRINT('-> Loading Time ' + CAST(DATEDIFF(SECOND,@start_time,@end_time) AS NVARCHAR)) + 'seconds';
+		PRINT('->');
 
-	TRUNCATE TABLE bronze.crm_sales_details;
-	BULK INSERT bronze.crm_sales_details
-	FROM 'C:\Users\sanid\Desktop\365 Plan\Skills\Data Analytics\SQL\Data Warehouse Project (CRM-ERP Data)\Datasets\source_crm\sales_details.csv' 
-	WITH (
-		FIRSTROW = 2,
-		FIELDTERMINATOR = ',',
-		TABLOCK
-	);
+		SET @start_time = GETDATE();
+		PRINT('-> Truncating Table: bronze.crm_prd_info');
+		TRUNCATE TABLE bronze.crm_prd_info;
+		PRINT('-> Inserting Data Into: bronze.crm_prd_info');
+		BULK INSERT bronze.crm_prd_info
+		FROM 'C:\Users\sanid\Desktop\365 Plan\Skills\Data Analytics\SQL\Data Warehouse Project (CRM-ERP Data)\Datasets\source_crm\prd_info.csv' 
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		SET @end_time = GETDATE();
+		PRINT('-> Loading Time ' + CAST(DATEDIFF(SECOND,@start_time,@end_time) AS NVARCHAR)) + 'seconds';
+		PRINT('->');
 
-	TRUNCATE TABLE bronze.erp_cust_az12;
-	BULK INSERT bronze.erp_cust_az12
-	FROM 'C:\Users\sanid\Desktop\365 Plan\Skills\Data Analytics\SQL\Data Warehouse Project (CRM-ERP Data)\Datasets\source_erp\CUST_AZ12.csv' 
-	WITH (
-		FIRSTROW = 2,
-		FIELDTERMINATOR = ',',
-		TABLOCK
-	);
+		SET @start_time = GETDATE();
+		PRINT('-> Truncating Table: bronze.crm_sales_details');
+		TRUNCATE TABLE bronze.crm_sales_details;
+		PRINT('-> Inserting Data Into: bronze.crm_sales_details');
+		BULK INSERT bronze.crm_sales_details
+		FROM 'C:\Users\sanid\Desktop\365 Plan\Skills\Data Analytics\SQL\Data Warehouse Project (CRM-ERP Data)\Datasets\source_crm\sales_details.csv' 
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		SET @end_time = GETDATE();
+		PRINT('-> Loading Time ' + CAST(DATEDIFF(SECOND,@start_time,@end_time) AS NVARCHAR)) + 'seconds';
+		PRINT('->');
 
-	TRUNCATE TABLE bronze.erp_loc_a101;
-	BULK INSERT bronze.erp_loc_a101
-	FROM  'C:\Users\sanid\Desktop\365 Plan\Skills\Data Analytics\SQL\Data Warehouse Project (CRM-ERP Data)\Datasets\source_erp\LOC_A101.csv'
-	WITH (
-		FIRSTROW = 2,
-		FIELDTERMINATOR = ',',
-		TABLOCK
-	);
+		PRINT('--------------------------------------------------------------');
+		PRINT('Loading ERP Tables');
+		PRINT('--------------------------------------------------------------');
+		SET @start_time = GETDATE();
+		PRINT('-> Truncating Table: bronze.erp_cust_az12');
+		TRUNCATE TABLE bronze.erp_cust_az12;
+		PRINT('-> Inserting Data Into: bronze.erp_cust_az12');
+		BULK INSERT bronze.erp_cust_az12
+		FROM 'C:\Users\sanid\Desktop\365 Plan\Skills\Data Analytics\SQL\Data Warehouse Project (CRM-ERP Data)\Datasets\source_erp\CUST_AZ12.csv' 
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		SET @end_time = GETDATE();
+		PRINT('-> Loading Time ' + CAST(DATEDIFF(SECOND,@start_time,@end_time) AS NVARCHAR)) + 'seconds';
+		PRINT('->');
 
-	TRUNCATE TABLE bronze.erp_px_cat_g1v2;
-	BULK INSERT bronze.erp_px_cat_g1v2
-	FROM 'C:\Users\sanid\Desktop\365 Plan\Skills\Data Analytics\SQL\Data Warehouse Project (CRM-ERP Data)\Datasets\source_erp\PX_CAT_G1V2.csv' 
-	WITH (
-		FIRSTROW = 2,
-		FIELDTERMINATOR = ',',
-		TABLOCK
-	);
+		SET @start_time = GETDATE();
+		PRINT('-> Truncating Table: bronze.erp_loc_a101');
+		TRUNCATE TABLE bronze.erp_loc_a101;
+		PRINT('-> Inserting Data Into: bronze.erp_loc_a101');
+		BULK INSERT bronze.erp_loc_a101
+		FROM  'C:\Users\sanid\Desktop\365 Plan\Skills\Data Analytics\SQL\Data Warehouse Project (CRM-ERP Data)\Datasets\source_erp\LOC_A101.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		SET @end_time = GETDATE();
+		PRINT('-> Loading Time ' + CAST(DATEDIFF(SECOND,@start_time,@end_time) AS NVARCHAR)) + 'seconds';
+		PRINT('->');
+
+		SET @start_time = GETDATE();
+		PRINT('-> Truncating Table: bronze.erp_px_cat_g1v2');
+		TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+		PRINT('-> Inserting Data Into: bronze.erp_px_cat_g1v2');
+		BULK INSERT bronze.erp_px_cat_g1v2
+		FROM 'C:\Users\sanid\Desktop\365 Plan\Skills\Data Analytics\SQL\Data Warehouse Project (CRM-ERP Data)\Datasets\source_erp\PX_CAT_G1V2.csv' 
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		SET @end_time = GETDATE();
+		PRINT('-> Loading Time ' + CAST(DATEDIFF(SECOND,@start_time,@end_time) AS NVARCHAR)) + 'seconds';
+
+		SET @batch_end_time = GETDATE();
+		PRINT('->Batch Loading Time ' + CAST(DATEDIFF(SECOND,@batch_start_time,@batch_end_time) AS NVARCHAR)) + 'seconds';
+
+		PRINT('==============================================================');
+	END TRY
+	BEGIN CATCH
+		PRINT('=======================================================');
+		PRINT('ERROR OCCURED DURING LOADING BRONZE LAYER');
+		PRINT('=======================================================');
+		PRINT('ERROR MESSAGE: '+ERROR_MESSAGE());
+		PRINT('ERROR NUMBER: '+ CAST (ERROR_NUMBER() AS NVARCHAR));
+		PRINT('ERROR STATE: '+ CAST (ERROR_STATE() AS NVARCHAR));
+		PRINT('=======================================================');
+	END CATCH
 END;
-
-EXEC bronze.load_bronze;
